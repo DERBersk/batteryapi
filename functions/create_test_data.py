e9a47a13f1c0e61221c63a6e0b0df3b6fd77cbbc
Dominik Eitner <76207875+DERBersk@users.noreply.github.com>
2024-05-10 22:46:41 +0200

    Create Data fix

--- functions/create_test_data.py
+++ functions/create_test_data.py
@@ -23,7 +23,7 @@ def populate_suppliers(num_suppliers=5):
         sustainability_index = random.uniform(0, 10)
         quality = random.uniform(0, 10)
         reliability = random.uniform(0,1)
-        availability = random.uniform(0,1)
+        availability = random.uniform(0,1)>0.5
         supplier = Supplier(name=name, lat=lat, long=long, risk_index=risk_index, sustainability_index=sustainability_index, quality=quality, reliability=reliability,availability=availability)
         db.session.add(supplier)
         db.session.commit()
