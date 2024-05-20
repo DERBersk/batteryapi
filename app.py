7590da782ee5766ead4cb28363c2d3ed495db8ef
Nour_Ashraf <nour.ashraf@webgenstudio.com>
2024-05-20 15:34:28 +0200

    Add CORS to all routes

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from extensions import db
+from flask_cors import CORS
 
 #TODO: Historical Demand Routes
 #TODO: Analytics Routes - First Mock then create
 
 def create_app():
     app = Flask(__name__.split(".")[0])
+    CORS(app)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres.xheevcowfsiqoblrojtv:TGEBzsP7RGjpVM5E@aws-0-eu-west-2.pooler.supabase.com:5432/postgres'
     # Import routes
     from routes.supplier_routes import supplier_bp
