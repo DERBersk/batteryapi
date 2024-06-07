9c02cc268b52bcccb5380d613d2d965c8139e9de
Dominik Eitner <76207875+DERBersk@users.noreply.github.com>
2024-06-07 14:23:17 +0200

    Config File and Data Submission

--- routes/external_routes.py
+++ routes/external_routes.py
@@ -1,23 +1,31 @@
 from datetime import datetime,timedelta
-from flask import Blueprint, request,render_template,current_app
+from flask import Blueprint, request,render_template,current_app,flash
 from functions.email import send_email
 from functions.token_manager import generate_token,tokens
+import json
+from extensions import db
 from models.supplier import Supplier
+from models.materials_per_supplier import MaterialsPerSupplier
+from models.material import Material
 
 external_bp = Blueprint('external', __name__, url_prefix='/api/external')
+
+# Load configuration from a JSON file
+with open('config.json', 'r') as file:
+    config = json.load(file)
 
 @external_bp.route('/generate_link/<int:supplier_id>',methods=["POST"])
 def generate_link(supplier_id):
     supplier = Supplier.query.filter(Supplier.id == supplier_id).first()
     if supplier:
         email = supplier.email #This email should be the email of the supplier
-        expiration_time = datetime.now() + timedelta(days=1)  # Change this to whatever expiration time you want
-        token = generate_token(email, expiration_time)
+        expiration_time = datetime.now() + timedelta(days=7)  # Change this to whatever expiration time you want
+        token = generate_token(email, expiration_time, supplier.id)
         link = request.host_url + 'api/external/update_data/' + token
 
         # Render link.html content
         with current_app.app_context():
-            html_content = render_template('link.html', link=link)
+            html_content = render_template('link.html', link=link, company_name=config['COMPANY_NAME'])
 
         # Send email
         subject = 'Update Your Supplier Data'
@@ -26,17 +34,60 @@ def generate_link(supplier_id):
         return 'Link sent to {}'.format(email)
     return "Failed"
 
-@external_bp.route('/update_data/<token>',methods=["GET","POST"])
+@external_bp.route('/update_data/<token>', methods=["GET", "POST"])
 def update_data(token):
     if token in tokens:
         supplier_data = tokens[token]
         if datetime.now() < supplier_data['expiration_time']:
+            supplier = Supplier.query.filter(Supplier.id == supplier_data['id']).first()
+            if not supplier:
+                return 'Invalid supplier.'
+
             if request.method == 'POST':
-                # Update supplier data here
-                # For example, you can access form data using request.form
+                # Retrieve form data
+                name = request.form.get('name')
+                email = request.form.get('email')
+
+                with db.session.no_autoflush:
+                    # Update supplier fields
+                    supplier.name = name
+                    supplier.email = email
+
+                    # Update materials data
+                    materials_per_supplier = MaterialsPerSupplier.query.filter_by(supplier_id=supplier.id).all()
+                    for material_per_supplier in materials_per_supplier:
+                        material_id = material_per_supplier.material_id
+                        lead_time = request.form.get(f'lead_time_{material_id}')
+                        co2_emissions = request.form.get(f'co2_emissions_{material_id}')
+
+                        if lead_time is not None:
+                            material_per_supplier.lead_time = lead_time
+                        if co2_emissions is not None:
+                            material_per_supplier.co2_emissions = co2_emissions
+
+                    # Commit changes to the database
+                    db.session.commit()
+
                 return 'Data updated successfully!'
-            else:
-                return render_template('update_form.html')
+
+            # Query materials associated with the supplier
+            materials = db.session.query(Material, MaterialsPerSupplier).\
+                        join(MaterialsPerSupplier, Material.id == MaterialsPerSupplier.material_id).\
+                        filter(MaterialsPerSupplier.supplier_id == supplier.id).\
+                        all()
+
+            # Prepare data for rendering the form
+            materials_data = []
+            for material, material_per_supplier in materials:
+                materials_data.append({
+                    'id': material.id,
+                    'name': material.name,
+                    'lead_time': material_per_supplier.lead_time,
+                    'co2_emissions': material_per_supplier.co2_emissions
+                })
+
+            # Render the form with existing supplier data and materials
+            return render_template('update_form.html', supplier=supplier, materials=materials_data)
         else:
             return 'Link has expired.'
     else:
